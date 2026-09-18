598180c6b745394fd2420f1589ce93816f17d098
xenoluz <187833508+Xenoluz@users.noreply.github.com>
2026-09-18 08:54:15 +0700

    diplaying enemy wave 1 to 100

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+for (int wave = 1; wave <= 100; wave++)
+{
+    Console.WriteLine($"On the wave {wave}.");
+    System.Threading.Thread.Sleep(300);
+    Console.WriteLine($"Starting wave {wave}, character is fighting..");
+    System.Threading.Thread.Sleep(600);
+    Console.WriteLine($"Closing wave {wave}, character is on the way to the next wave.\n");
+    System.Threading.Thread.Sleep(300);
+}
